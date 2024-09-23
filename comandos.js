b1acf918d24ebcf25d2c63d1fb9923f68d2eab58
Lucas Machado Araujo <lucas.machado@einstein.br>
2024-09-23 17:07:09 -0300

    add dragons imagen

--- comandos.js
+++ comandos.js
@@ -1,4 +1,4 @@
-let nome = prompt("Qual o seu nome cavaleiro?");
-alert(`seja bem vindo, Lord ${nome}!`);
+/*let nome = prompt("Qual o seu nome cavaleiro?");
+alert(`seja bem vindo, Lord ${nome}!`);*/
 
 /*document.write(`lord ${nome}`);*/
