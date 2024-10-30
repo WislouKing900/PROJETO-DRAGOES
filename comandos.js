010632694771690a32d8c007ded2c2436dbfcca8
lucas machado <machado.lucas.900@gmail.com>
2024-10-29 23:11:00 -0300

    separando o selo para monarcas

--- comandos.js
+++ comandos.js
@@ -1,4 +1,11 @@
-/*let nome = prompt("Qual o seu nome cavaleiro?");
-alert(`seja bem vindo, Lord ${nome}!`);*/
+var nome = prompt(`Quem é você ?`);
+alert(`Seja Bem-Vindo(a) ${nome}`)
 
-/*document.write(`lord ${nome}`);*/
+var insignia = window.document.getElementById('insignia')
+
+if (nome.includes('\__|__/')){
+    insignia.style.display = 'flex';
+}else{
+    insignia.style.display = 'none'
+}
+
