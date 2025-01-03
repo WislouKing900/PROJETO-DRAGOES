8a4f086bf6cc40e8bd7e16b7fe324f09679a6399
lucas machado <machado.lucas.900@gmail.com>
2025-01-02 22:51:13 -0300

    tentativa 2 insi_desaparecer

--- comandos.js
+++ comandos.js
@@ -1,9 +1,9 @@
 var nome = prompt(`Quem vem lá ?`);
 alert(`Seja Bem-Vindo(a) ${nome}`)
 
-if (nome = ''){
-    window.document.body.style.background = 'black'
-}
+/*if (nome = ''){
+    window.document.html = 'none'
+}*/
 
 var insignia = window.document.getElementById('insignia')
 
