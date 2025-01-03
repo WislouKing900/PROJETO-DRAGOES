7f1d89954cbbfc733431bd773f283b0e5e505ab8
Lucas Machado Araujo <lucas.machado@einstein.br>
2025-01-03 11:18:07 -0300

    corrigindo um bug no comandos.js

--- comandos.js
+++ comandos.js
@@ -4,6 +4,7 @@ alert(`Seja Bem-Vindo(a) ${nome}`)
 var insignia = window.document.getElementById('insignia')
 
 if (nome.includes('\__|__/')){
+    
     insignia.style.display = 'flex';
 }else{
     insignia.style.display = 'none'
