c367764a7e9d2c08588b2fa7c565ba9ec3aa54f4
lucas machado <machado.lucas.900@gmail.com>
2025-10-17 13:29:56 -0300

    reparos

---
+++ comandos.js
@@ -0,0 +1,11 @@
+/*var nome = prompt('Quem ousa bater nos portões de Drakarius?')
+
+alert (`Ah, Lorede(a) ${nome}, seja Bem-vindo(a)`)
+
+var selo = window.document.getElementById('insignia')
+
+if (nome.includes('\_|_/')){
+    selo.style.display = 'flex'
+}else{
+    selo.style.display = 'none'
+}*/
